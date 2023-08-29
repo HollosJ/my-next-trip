5ec2718020705c638f7498bcf2e267eab7092c5a
HollosJ <hollosj99@gmail.com>
2023-08-29 19:21:40 +0100

    Landing page

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,41 @@
+import Image from 'next/image';
+import HeroImage from '@/public/images/hero-image.svg';
+import PlaneImage from '@/public/images/plane.svg';
+import TrailImage from '@/public/images/trail.svg';
+import Link from 'next/link';
 import Heading from '@/components/Heading';
-import Trips from '@/components/Trips';
 
 export default function Home() {
   return (
-    <section className="container my-8 md:max-w-screen-md">
-      <Heading>Trips</Heading>
+    <>
+      {/* Hero section */}
+      <div className="py-8">
+        <div className="container grid items-center gap-8 md:grid-cols-2 md:max-w-screen-lg">
+          <div className="grid gap-8">
+            <h1 className="text-3xl font-bold lg:text-5xl">
+              Your{' '}
+              <span className="text-transparent gradient--green bg-clip-text">
+                Ultimate
+              </span>{' '}
+              Travel Planning Companion!
+            </h1>
 
-      <Trips />
-    </section>
+            <Link
+              href={'#'}
+              className="button button--primary justify-self-start"
+            >
+              Get Started
+            </Link>
+          </div>
+
+          <Image
+            src={HeroImage}
+            width={640}
+            height={424}
+            alt="Map with travel related items atop"
+          />
+        </div>
+      </div>
+    </>
   );
 }
