b449f70a5e5e207215343268dc92a3b772aa4d69
HollosJ <hollosj99@gmail.com>
2023-08-29 19:22:51 +0100

    Endpoint to create a new trip

--- app/api/trip/new/route.js
+++ app/api/trip/new/route.js
@@ -1,17 +1,31 @@
-import Trip from "@/models/trip";
-import { connectToDB } from "@/utils/database";
+import Trip from '@/models/trip';
+import { connectToDB } from '@/utils/database';
 
-export const POST = async (req) => {
-  const { userID, location, startDate, endDate } = await req.json();
+export const POST = async (request) => {
+  const { userID, location, startDate, endDate } = await request.json();
 
   try {
     await connectToDB();
+
+    // Generate date between start and end of trip (inclusive)
+    let currentDate = new Date(startDate);
+    let lastDate = new Date(endDate);
+
+    // Initialize dates array
+    let dates = [];
+
+    // Loop through every date between start and end, then generate a new object
+    while (currentDate <= lastDate) {
+      dates.push({ date: new Date(currentDate), activities: [] });
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
 
     const newTrip = new Trip({
       user: userID,
       location,
       startDate,
       endDate,
+      itinerary: dates,
     });
 
     await newTrip.save();
@@ -19,6 +33,6 @@ export const POST = async (req) => {
     return new Response(JSON.stringify(newTrip), { status: 201 });
   } catch (error) {
     console.error(error);
-    return new Response("Failed to create trip", { status: 500 });
+    return new Response('Failed to create trip', { status: 500 });
   }
 };
