28a19d90f4e75f1a8e748827bf724402ecb33d8d
HollosJ <hollosj99@gmail.com>
2023-08-29 19:25:34 +0100

    Move trips grid to own component

---
+++ components/TripsGrid.jsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
+import React, { useState, useEffect } from 'react';
+import Protected from './Protected';
+import Link from 'next/link';
+import Heading from './Heading';
+
+const TripsGrid = () => {
+  const { data: session } = useSession();
+  const [trips, setTrips] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTrips = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/trips`);
+      const data = await response.json();
+
+      setTrips(data);
+      setLoading(false);
+    };
+
+    if (session?.user.id) fetchTrips();
+  }, [session?.user.id]);
+  return (
+    <div className="grid gap-8">
+      <Heading>My Trips</Heading>
+
+      <Protected session={session}>
+        <div className="grid gap-4">
+          {loading ? (
+            'Loading your trips...'
+          ) : trips.length ? (
+            <div className="grid gap-4">
+              <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
+                {trips.map((trip) => (
+                  <Link
+                    className="p-4 bg-white rounded shadow-md"
+                    key={trip._id}
+                    href={`/trips/${trip._id}`}
+                  >
+                    {trip.location}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          ) : (
+            <span>You have no trips yet!</span>
+          )}
+
+          <Link className="button button--primary" href={'/trips/new'}>
+            Add New
+          </Link>
+        </div>
+      </Protected>
+    </div>
+  );
+};
+
+export default TripsGrid;
