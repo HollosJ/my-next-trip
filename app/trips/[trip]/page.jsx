1dcdf986ec56506f58c6d1d21f8d7534fd0ded4b
HollosJ <hollosj99@gmail.com>
2023-08-30 19:45:53 +0100

    Convert svgs to heroicons

--- app/trips/[trip]/page.jsx
+++ app/trips/[trip]/page.jsx
@@ -6,6 +6,7 @@ import PointsOfInterestGrid from '@/components/PointsOfInterestGrid';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import { ClockIcon } from '@heroicons/react/24/solid';
 
 const Trip = ({ params }) => {
   const { data: session } = useSession();
@@ -70,20 +71,7 @@ const Trip = ({ params }) => {
 
               {daysTill > 0 && (
                 <div className="flex items-center gap-2 p-2 text-white rounded bg-gradient-to-tr from-green-700 to-green-500">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg>
+                  <ClockIcon className="w-6 h-6" />
 
                   <p>Only {daysTill} days away!</p>
                 </div>
