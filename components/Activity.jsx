6f22c8587046eaeebff203d0c3e180eeb1c44401
HollosJ <hollosj99@gmail.com>
2023-09-06 14:15:09 +0100

    Change action styling so they arent spaced out when large amount of content

--- components/Activity.jsx
+++ components/Activity.jsx
@@ -20,19 +20,22 @@ const Activity = ({ activity, onSave, handleDeleteActivity }) => {
         />
       ) : (
         <div className="relative flex border-2 rounded">
+          {/* Activity content */}
           <div className="grid flex-1 p-4">
             <h3 className="text-lg font-bold">{activity.title}</h3>
             {activity.location && <span>{activity.location}</span>}
             {activity.notes && <p className="opacity-75">"{activity.notes}"</p>}
           </div>
 
-          <div className="grid gap-2 p-4 border-l-2">
+          {/* Actions */}
+          <div className="grid content-start gap-1 p-1">
             <button
               className="underline"
               onClick={() => handleDeleteActivity(activity._id)}
             >
               <TrashIcon className="w-6 h-6 text-red-400 transition hover:opacity-75" />
             </button>
+
             <button className="underline" onClick={() => setFormShowing(true)}>
               <PencilSquareIcon className="w-6 h-6 text-green-400 transition hover:opacity-75" />
             </button>
