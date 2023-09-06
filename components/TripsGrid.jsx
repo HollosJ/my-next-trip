3321f18ed4381351d54502d246cce96a116115fa
HollosJ <hollosj99@gmail.com>
2023-09-06 20:16:13 +0100

    Add error state for user in case of fetch error

--- components/TripsGrid.jsx
+++ components/TripsGrid.jsx
@@ -12,24 +12,30 @@ const TripsGrid = () => {
   const { data: session } = useSession();
   const [trips, setTrips] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
-    const fetchTrips = async () => {
-      const response = await fetch(`/api/users/${session?.user.id}/trips`);
-      const data = await response.json();
-
-      const dataSorted = data.sort(function (a, b) {
-        // Turn your strings into dates, and then subtract them
-        // to get a value that is either negative, positive, or zero.
-        return new Date(a.startDate) - new Date(b.startDate);
-      });
-
-      setTrips(dataSorted);
-      setLoading(false);
-    };
-
     if (session?.user.id) fetchTrips();
   }, [session?.user.id]);
+
+  const fetchTrips = async () => {
+    const response = await fetch(`/api/users/${session?.user.id}/trips`);
+
+    if (!response.ok) setError(true);
+    else setError(false);
+
+    const data = await response.json();
+
+    const dataSorted = data.sort(function (a, b) {
+      // Turn your strings into dates, and then subtract them
+      // to get a value that is either negative, positive, or zero.
+      return new Date(a.startDate) - new Date(b.startDate);
+    });
+
+    setTrips(dataSorted);
+    setLoading(false);
+  };
+
   return (
     <Protected session={session}>
       <div className="grid gap-8">
@@ -40,19 +46,34 @@ const TripsGrid = () => {
             'Loading your trips...'
           ) : trips.length ? (
             <div className="grid gap-4">
-              <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
-                {trips.map((trip) => (
-                  <Link
-                    className="p-4 transition bg-white rounded shadow-md hover:opacity-75"
-                    key={trip._id}
-                    href={`/trips/${trip._id}`}
-                  >
-                    <h3 className="font-bold">{trip.location}</h3>
+              {error ? (
+                <div className="flex flex-wrap items-center gap-2">
+                  <p className="font-bold text-red-700">
+                    Error getting your trips from the database
+                  </p>
 
-                    <span>{formatDate(trip.startDate)}</span>
-                  </Link>
-                ))}
-              </div>
+                  <button
+                    className="underline button button--primary"
+                    onClick={fetchTrips}
+                  >
+                    Retry?
+                  </button>
+                </div>
+              ) : (
+                <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
+                  {trips.map((trip) => (
+                    <Link
+                      className="p-4 transition bg-white rounded shadow-md hover:opacity-75"
+                      key={trip._id}
+                      href={`/trips/${trip._id}`}
+                    >
+                      <h3 className="font-bold">{trip.location}</h3>
+
+                      <span>{formatDate(trip.startDate)}</span>
+                    </Link>
+                  ))}
+                </div>
+              )}
             </div>
           ) : (
             <span>You have no trips yet!</span>
