d891e8bac171b3b4d3156831e8df28bbdb9fdfba
HollosJ <hollosj99@gmail.com>
2023-09-06 13:56:31 +0100

    Move protected component to outside to prevent issue where signed out on trip page puts user into endless loading skeletons

--- app/trips/[trip]/page.jsx
+++ app/trips/[trip]/page.jsx
@@ -39,29 +39,31 @@ const Trip = ({ params }) => {
   return (
     <>
       <div className="container my-8 md:max-w-screen-md">
-        {loading ? (
-          <div className="flex flex-wrap items-center justify-between gap-4">
-            <Skeleton className={'h-10 w-64'} />
-
-            <Skeleton className={'h-10 w-48'} />
-          </div>
-        ) : (
-          <Protected session={session}>
+        <Protected session={session}>
+          {loading ? (
             <div className="flex flex-wrap items-center justify-between gap-4">
-              <Heading>Your Trip To {trip.location}</Heading>
+              <Skeleton className={'h-10 w-64'} />
 
-              {daysTill > 0 && (
-                <div className="flex items-center gap-2 p-2 text-white rounded bg-gradient-to-tr from-green-700 to-green-500">
-                  <ClockIcon className="w-6 h-6" />
-
-                  <p>Only {daysTill} days away!</p>
-                </div>
-              )}
+              <Skeleton className={'h-10 w-48'} />
             </div>
+          ) : (
+            <>
+              <div className="flex flex-wrap items-center justify-between gap-4">
+                <Heading>Your Trip To {trip.location}</Heading>
 
-            <PointsOfInterestGrid trip={trip} setTrip={setTrip} />
-          </Protected>
-        )}
+                {daysTill > 0 && (
+                  <div className="flex items-center gap-2 p-2 text-white rounded bg-gradient-to-tr from-green-700 to-green-500">
+                    <ClockIcon className="w-6 h-6" />
+
+                    <p>Only {daysTill} days away!</p>
+                  </div>
+                )}
+              </div>
+
+              <PointsOfInterestGrid trip={trip} setTrip={setTrip} />
+            </>
+          )}
+        </Protected>
       </div>
     </>
   );
