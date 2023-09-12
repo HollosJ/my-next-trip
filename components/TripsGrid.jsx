0ad7a8fefa368b7f3897417eec015a4e0dd55328
HollosJ <hollosj99@gmail.com>
2023-09-12 11:49:07 +0100

    Fix loading bug

--- components/TripsGrid.jsx
+++ components/TripsGrid.jsx
@@ -11,35 +11,25 @@ const TripsGrid = () => {
   const { data: session } = useSession();
   const [trips, setTrips] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
 
   useEffect(() => {
-    if (session?.user.id) fetchTrips();
+    const fetchTrips = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/trips`);
+
+      const data = await response.json();
+
+      const dataSorted = data.sort(function (a, b) {
+        // Turn your strings into dates, and then subtract them
+        // to get a value that is either negative, positive, or zero.
+        return new Date(a.startDate) - new Date(b.startDate);
+      });
+
+      setTrips(dataSorted);
+      setLoading(false);
+    };
+
+    if (session?.user) fetchTrips();
   }, [session?.user.id]);
-
-  const fetchTrips = async () => {
-    const response = await fetch(`/api/users/${session?.user.id}/trips`);
-
-    if (!response.ok) {
-      setError(true);
-      console.error(response.body);
-
-      return;
-    }
-
-    setError(false);
-
-    const data = await response.json();
-
-    const dataSorted = data.sort(function (a, b) {
-      // Turn your strings into dates, and then subtract them
-      // to get a value that is either negative, positive, or zero.
-      return new Date(a.startDate) - new Date(b.startDate);
-    });
-
-    setTrips(dataSorted);
-    setLoading(false);
-  };
 
   return (
     <div className="grid gap-8">
@@ -50,34 +40,19 @@ const TripsGrid = () => {
           "Loading your trips..."
         ) : trips.length ? (
           <div className="grid gap-4">
-            {error ? (
-              <div className="flex flex-wrap items-center gap-2">
-                <p className="font-bold text-red-700">
-                  Error getting your trips from the database
-                </p>
-
-                <button
-                  className="underline button button--primary"
-                  onClick={fetchTrips}
+            <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
+              {trips.map((trip) => (
+                <Link
+                  className="p-4 transition bg-white rounded shadow-md hover:opacity-75"
+                  key={trip._id}
+                  href={`/trips/${trip._id}`}
                 >
-                  Retry?
-                </button>
-              </div>
-            ) : (
-              <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
-                {trips.map((trip) => (
-                  <Link
-                    className="p-4 transition bg-white rounded shadow-md hover:opacity-75"
-                    key={trip._id}
-                    href={`/trips/${trip._id}`}
-                  >
-                    <h3 className="font-bold">{trip.location}</h3>
+                  <h3 className="font-bold">{trip.location}</h3>
 
-                    <span>{formatDate(trip.startDate)}</span>
-                  </Link>
-                ))}
-              </div>
-            )}
+                  <span>{formatDate(trip.startDate)}</span>
+                </Link>
+              ))}
+            </div>
           </div>
         ) : (
           <span>You have no trips yet!</span>
