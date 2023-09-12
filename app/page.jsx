8c497b43d48e78631643a14a6fae0a25fbc6c55d
HollosJ <hollosj99@gmail.com>
2023-09-12 15:57:06 +0100

    Redirect user to the trips page if theyre logged in

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
-import HeroImage from "@/public/images/hero-image.svg";
-import Link from "next/link";
-import { getServerSession } from "next-auth";
+import Image from 'next/image';
+import HeroImage from '@/public/images/hero-image.svg';
+import Link from 'next/link';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 
 export default async function Home() {
   const session = await getServerSession();
+
+  if (session?.user) redirect('/trips');
 
   return (
     <>
@@ -13,10 +16,10 @@ export default async function Home() {
         <div className="container grid items-center gap-8 md:grid-cols-2 md:max-w-screen-lg">
           <div className="grid gap-8 justify-items-start">
             <h1 className="text-3xl font-bold lg:text-5xl">
-              Your{" "}
+              Your{' '}
               <span className="text-transparent gradient--green bg-clip-text">
                 Ultimate
-              </span>{" "}
+              </span>{' '}
               Travel Planning Companion!
             </h1>
 
