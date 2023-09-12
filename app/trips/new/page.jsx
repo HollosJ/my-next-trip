8ed636a153cd225e8136d53393dbdbf643c92016
HollosJ <hollosj99@gmail.com>
2023-09-12 19:51:32 +0100

    Move all trip form logic into form, make page server component again

--- app/trips/new/page.jsx
+++ app/trips/new/page.jsx
@@ -1,74 +1,17 @@
-"use client";
+import TripForm from '@/components/TripForm';
+import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth';
 
-import { useState } from "react";
-import { useSession } from "next-auth/react";
-import { redirect, useRouter } from "next/navigation";
-import TripForm from "@/components/TripForm";
-
-const NewTrip = () => {
-  const router = useRouter();
-  const { data: session } = useSession();
+const NewTrip = async () => {
+  const session = await getServerSession();
 
   if (!session || !session?.user) {
-    redirect("/api/auth/signin");
+    redirect('/api/auth/signin');
   }
-
-  const [submitting, setSubmitting] = useState(false);
-  const [trip, setTrip] = useState({
-    location: "",
-    startDate: "",
-    endDate: "",
-    itinerary: [],
-  });
-
-  const createTrip = async (e) => {
-    e.preventDefault();
-
-    if (!trip.location) {
-      alert("Please select a country from the list...");
-
-      return;
-    }
-
-    if (trip.startDate > trip.endDate) {
-      alert("Leaving date must be after arrival date...");
-
-      return;
-    }
-
-    // Set 'loading' state so we can disable submit button once pressed
-    setSubmitting(true);
-
-    try {
-      const response = await fetch("/api/trip/new", {
-        method: "POST",
-        body: JSON.stringify({
-          location: trip.location,
-          userID: session?.user.id,
-          startDate: trip.startDate,
-          endDate: trip.endDate,
-        }),
-      });
-
-      if (response.ok) {
-        router.push("/trips");
-      }
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setSubmitting(false);
-    }
-  };
 
   return (
     <div className="container my-8 md:max-w-screen-md">
-      <TripForm
-        type="Create"
-        trip={trip}
-        setTrip={setTrip}
-        submitting={submitting}
-        handleSubmit={createTrip}
-      />
+      <TripForm type="Create" />
     </div>
   );
 };
--- components/TripForm.jsx
+++ components/TripForm.jsx
@@ -1,37 +1,103 @@
+'use client';
+
 import Link from 'next/link';
 import Heading from './Heading';
 import { countries } from '@/public/countries';
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
-const Form = ({ type, trip, setTrip, submitting, handleSubmit }) => {
+const Form = ({ type }) => {
+  const { data: session } = useSession();
+  const router = useRouter();
+  const [submitting, setSubmitting] = useState(false);
+  const [trip, setTrip] = useState({
+    location: '',
+    startDate: '',
+    endDate: '',
+    itinerary: [],
+  });
+
+  const createTrip = async (e) => {
+    e.preventDefault();
+
+    if (!trip.location) {
+      alert('Please select a country from the list...');
+
+      return;
+    }
+
+    if (trip.startDate > trip.endDate) {
+      alert('Leaving date must be after arrival date...');
+
+      return;
+    }
+
+    // Set 'loading' state so we can disable submit button once pressed
+    setSubmitting(true);
+
+    try {
+      const response = await fetch('/api/trip/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          location: trip.location,
+          userID: session?.user.id,
+          startDate: trip.startDate,
+          endDate: trip.endDate,
+        }),
+      });
+
+      if (response.ok) {
+        router.push('/trips');
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
   return (
     <section className="grid gap-8 p-4 bg-white rounded shadow-md">
       <Heading>{type} Trip</Heading>
 
-      <form className="grid gap-8 whitespace-normal" onSubmit={handleSubmit}>
+      <form className="grid gap-8 whitespace-normal" onSubmit={createTrip}>
         {/* Location */}
-        <div className="grid">
-          <label htmlFor="location">Where are you going?</label>
+        <div className="grid gap-4">
+          <div className="grid">
+            <label htmlFor="location">Where are you going?</label>
 
-          <select
-            className="input"
-            name="location"
-            id="location"
-            value={trip.location}
-            onChange={(e) =>
-              setTrip({
-                ...trip,
-                location: e.target.value,
-              })
-            }
-          >
-            <option value="" disabled>
-              Please select...
-            </option>
+            <select
+              className="input"
+              name="location"
+              id="location"
+              value={trip.location}
+              onChange={(e) =>
+                setTrip({
+                  ...trip,
+                  location: e.target.value,
+                })
+              }
+            >
+              <option value="" disabled>
+                Please select...
+              </option>
 
-            {countries.map((country) => (
-              <option key={country}>{country}</option>
-            ))}
-          </select>
+              {countries.map((country) => (
+                <option key={country}>{country}</option>
+              ))}
+            </select>
+          </div>
+
+          <div className="flex items-center w-full gap-4">
+            <hr className="flex-1" />
+
+            <span>OR</span>
+
+            <hr className="flex-1" />
+          </div>
+
+          <input className="input" type="text" />
         </div>
 
         <div className="grid gap-8 md:gap-4 md:grid-cols-2">
