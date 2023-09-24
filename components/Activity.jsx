1fec23209ad80f95f383bd956fae9e115b3e472d
HollosJ <hollosj99@gmail.com>
2023-09-24 16:17:20 +0100

    Style changes so that edit form is within card

--- components/Activity.jsx
+++ components/Activity.jsx
@@ -12,47 +12,54 @@ const Activity = ({ activity, onSave, handleDeleteActivity }) => {
   return (
     <div className="text-black rounded">
       {/* Show activity details */}
-      {formShowing ? (
-        <ActivityForm
-          activity={activity}
-          onSave={handleSave}
-          setFormShowing={setFormShowing}
-        />
-      ) : (
-        <div className="relative flex whitespace-normal bg-white rounded shadow-md">
-          {/* Activity content */}
-          <div className="grid flex-1 p-4">
-            <h3 className="text-lg font-bold">{activity.title}</h3>
-
-            {activity.location && <span>{activity.location}</span>}
-
-            {activity.notes && (
-              <p className="break-all opacity-75">"{activity.notes}"</p>
-            )}
+      <div className="whitespace-normal bg-white rounded shadow-md">
+        {formShowing ? (
+          <div className="grid p-4">
+            <ActivityForm
+              activity={activity}
+              onSave={handleSave}
+              setFormShowing={setFormShowing}
+            />
           </div>
+        ) : (
+          <div className="relative flex">
+            {/* Activity content */}
+            <div className="grid flex-1 p-4">
+              <h3 className="text-lg font-bold">{activity.title}</h3>
 
-          {/* Actions */}
-          <div className="grid content-start gap-1 p-4 pl-0">
-            <button
-              className="underline"
-              onClick={() => {
-                if (
-                  window.confirm(
-                    'Are you sure you want to delete this activity?'
+              {activity.location && <span>{activity.location}</span>}
+
+              {activity.notes && (
+                <p className="break-all opacity-75">"{activity.notes}"</p>
+              )}
+            </div>
+
+            {/* Actions */}
+            <div className="grid content-start gap-1 p-4 pl-0">
+              <button
+                className="underline"
+                onClick={() => {
+                  if (
+                    window.confirm(
+                      'Are you sure you want to delete this activity?'
+                    )
                   )
-                )
-                  handleDeleteActivity(activity._id);
-              }}
-            >
-              <TrashIcon className="w-6 h-6 transition text-slate-300 hover:brightness-90" />
-            </button>
+                    handleDeleteActivity(activity._id);
+                }}
+              >
+                <TrashIcon className="w-6 h-6 transition text-slate-300 hover:brightness-90" />
+              </button>
 
-            <button className="underline" onClick={() => setFormShowing(true)}>
-              <PencilSquareIcon className="w-6 h-6 transition text-slate-300 hover:brightness-90" />
-            </button>
+              <button
+                className="underline"
+                onClick={() => setFormShowing(true)}
+              >
+                <PencilSquareIcon className="w-6 h-6 transition text-slate-300 hover:brightness-90" />
+              </button>
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
