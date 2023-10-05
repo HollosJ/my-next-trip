f477f1b3d67ff6c39b75e986f44286f48692b1e1
HollosJ <hollosj99@gmail.com>
2023-10-05 13:21:12 +0100

    Margin to loading wheel (doesnt work yet)

--- app/loading.jsx
+++ app/loading.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const loading = () => {
   return (
-    <div className="container flex items-center justify-center md:max-w-screen-md">
+    <div className="container flex items-center justify-center my-8 md:max-w-screen-md">
       {/* Spinner */}
       <div className="w-16 h-16 border-2 rounded-full border-x-transparent animate-spin"></div>
     </div>
