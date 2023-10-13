e8c44b08dfa5cc303dd9b059208668722d05bdcf
HollosJ <hollosj99@gmail.com>
2023-10-13 13:03:01 +0100

    Break component into various

--- components/Trip.jsx
+++ components/Trip.jsx
@@ -1,70 +1,75 @@
 'use client';
 
 import DateColumns from './DateColumns';
-import { ClockIcon } from '@heroicons/react/24/solid';
+import { ArrowUturnLeftIcon, ClockIcon } from '@heroicons/react/24/solid';
 import ErrorImage from '@/public/images/error-image.svg';
 import Link from 'next/link';
 import Image from 'next/image';
 import Heading from './Heading';
+import { toast } from 'sonner';
 
 const Trip = ({ trip, setTrip, loading, error, daysTill, deleteTrip }) => {
   return (
     <div className="grid flex-auto h-full">
       {error ? (
-        <div className="container grid content-start max-w-sm gap-4 my-8 text-red-500 justify-items-center">
-          <Image
-            src={ErrorImage}
-            alt="Error image"
-            width={1120}
-            height={700}
-            priority
-          />
-
-          <Heading>Oops!</Heading>
-
-          <span>{error}</span>
-
-          <Link className="button button--primary" href="/trips">
-            Go back to My Trips
-          </Link>
-        </div>
+        <Error error={error} />
       ) : (
         <div className="grid content-start grid-rows-[min-content,1fr] md:grid-rows-1 md:content-stretch md:grid-cols-[min-content,1fr]">
           {/* Dashboard header */}
-          <div className="grid content-start gap-4 p-4 text-white bg-slate-950 md:w-64">
-            {/* Title */}
-            <div className="flex flex-wrap items-center gap-2 overflow-hidden">
-              <span>Your trip to</span>
+          <div className="grid content-between gap-4 p-4 text-white bg-slate-950 md:w-64">
+            {/* Top */}
+            <div className="grid gap-4">
+              {/* Back button */}
+              <Link
+                className="flex gap-2 underline transition hover:opacity-75"
+                href="/trips"
+              >
+                <ArrowUturnLeftIcon className="w-6 h-6" />
 
-              <h1>
-                {loading ? (
-                  <div className="h-8 rounded w-36 bg-slate-200 animate-pulse"></div>
-                ) : (
-                  <span
-                    className={`text-transparent text-2xl break-words gradient--green font-bold bg-clip-text ${
-                      trip.location.length <= 10 ? 'md:text-4xl' : 'md:text-xl'
-                    }`}
-                  >
-                    {trip.location}
-                  </span>
-                )}
-              </h1>
-            </div>
+                <span>Trips</span>
+              </Link>
 
-            <div className="flex flex-wrap gap-4">
+              {/* Title */}
+              <div className="grid gap-2 overflow-hidden">
+                <span>Your trip to</span>
+
+                <h1>
+                  {loading ? (
+                    <div className="h-8 rounded w-36 bg-slate-200 animate-pulse"></div>
+                  ) : (
+                    <span
+                      className={`text-transparent text-2xl break-all gradient--green font-bold bg-clip-text`}
+                    >
+                      {trip.location}
+                    </span>
+                  )}
+                </h1>
+              </div>
+
+              {/* Days till */}
               {daysTill > 0 && !loading && (
-                <div className="flex items-center gap-2 p-2 text-black bg-white rounded">
-                  <ClockIcon className="w-6 h-6" />
-
-                  <p>Only {daysTill} days away!</p>
+                <div className="flex items-center gap-2 p-2 rounded justify-self-start md:justify-self-stretch gradient--green">
+                  <ClockIcon className="w-6 h-6" /> Only {daysTill} days away!
                 </div>
               )}
+            </div>
 
-              {!loading && (
-                <button className="button button--danger" onClick={deleteTrip}>
-                  Delete
-                </button>
-              )}
+            {/* Bottom */}
+            <div className="flex flex-wrap items-center gap-4">
+              {/* Delete button */}
+              <button
+                className="text-red-500 underline"
+                onClick={() =>
+                  toast('Are you sure you want to delete this trip?', {
+                    action: {
+                      label: 'Delete',
+                      onClick: () => deleteTrip(trip.id),
+                    },
+                  })
+                }
+              >
+                Delete trip
+              </button>
             </div>
           </div>
 
@@ -72,6 +77,28 @@ const Trip = ({ trip, setTrip, loading, error, daysTill, deleteTrip }) => {
           <DateColumns trip={trip} setTrip={setTrip} />
         </div>
       )}
+    </div>
+  );
+};
+
+const Error = ({ error }) => {
+  return (
+    <div className="container grid content-start max-w-sm gap-4 my-8 text-red-500 justify-items-center">
+      <Image
+        src={ErrorImage}
+        alt="Error image"
+        width={1120}
+        height={700}
+        priority
+      />
+
+      <Heading>Oops!</Heading>
+
+      <span>{error}</span>
+
+      <Link className="button button--primary" href="/trips">
+        Go back to Trips
+      </Link>
     </div>
   );
 };
